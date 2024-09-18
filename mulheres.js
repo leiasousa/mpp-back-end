4892b661aecd7eb8e3ac816b562450b213e94560
leiasousa <leiassousa2008@gmail.com>
2024-09-17 21:36:21 -0300

    adicionando a primeira versão do meu projeto

---
+++ mulheres.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const router = express.Router()
+
+const app = express()
+
+const porta = 3333
+
+const mulheres = [
+    {
+        nome: "Simara Conceição",
+        imagem: "https://bit.ly/3LJIyOF",
+        minibio: "Desenvolvedora e instrutora"
+    },
+    {
+        nome: "Iana Chan",
+        imagem: "https://bit.ly/3JCXBqP", 
+        minibio: "Fundadora da PrograMaria"
+    },
+    {
+        nome: "Nina da Hora",
+        imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5FqpPWzX7Ic0RhEoOpQYTk40dMHwBGGqqKD0wpK0vKeFNnzka",
+        minibio: "Hacker antirracista"
+    },
+    {
+
+        nome: 'Luana Pimentel',
+        imagem: 'https://bit.ly/3FKpFaz',
+        minibio: 'Senior Staff Software Engineer',
+     
+      }
+]
+
+function mostraMulheres(request, response){
+    response.json(mulheres)
+}
+
+function mostraPorta(){
+    console.log("Servidor criado e rodando na porta ", porta)
+}
+app.use(router.get("/mulheres", mostraMulheres))
+app.listen(porta, mostraPorta)
